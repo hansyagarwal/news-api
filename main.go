062b99bcd262e6ce6809fd2644c6a4ed9565d2e0
ARYAN AGARWAL <hansy.agarwal@gmail.com>
2020-11-08 02:10:07 +0530

    return all

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +16,47 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", home)
+
+	http.HandleFunc("/articles", returnArticle)
+	http.HandleFunc("/articles/1", returnById)
+
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
+type Article struct {
+	Id       int    `json:"Id"`
+	Title    string `json: "title"`
+	Subtitle string `json: "subtitle"`
+	Content  string `json: "content"`
+	//CreationTime
+}
+
+var Articles []Article
+
+func returnArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("endpoint hit: returnArticle")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnById(w http.ResponseWriter, r *http.Request) {
+	i := r.RequestURI
+	a := r.GetBody
+	if strings.HasPrefix(i, "/articles/") {
+		pe := url.PathEscape(strings.TrimLeft(i, "/articles/"))
+		fmt.Println(pe)
+		fmt.Println("endpoint hit: returnById")
+		json.NewEncoder(w).Encode(Articles)
+	}
+	fmt.Println("endpoint hit: returnArticle")
+
+}
+
 func main() {
+
+	Articles = []Article{
+		Article{Id: 1, Title: "Hello", Subtitle: "Sub hello", Content: "content1"},
+		Article{Id: 2, Title: "Hello 2", Subtitle: "Sub hello 2", Content: "content2"},
+	}
+
 	handleRequests()
 }
