8cc6c943ba10fe10c1d7115229541ffb771df433
ARYAN AGARWAL <hansy.agarwal@gmail.com>
2020-11-08 22:50:38 +0530

    demo cleanup

---
+++ demo.go
@@ -0,0 +1,63 @@
+//code i used for demo
+//P.S dont run this file
+
+/*
+type Article struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title    string             `json: "title,omitempty" bson:"title,omitempty"`
+	Subtitle string             `json: "subtitle,omitempty" bson:"subtitle,omitempty"`
+	Content  string             `json: "content,omitempty" bson:"content,omitempty"`
+}
+*/
+
+/*
+func returnArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("endpoint hit: returnArticle")
+
+	json.NewEncoder(w).Encode(Articles)
+}
+*/
+
+/*
+	func main() {
+		Articles = []Article{
+			Article{Id: "1", Title: "Hello", Subtitle: "Sub hello", Content: "content1"},
+			Article{Id: "2", Title: "Hello 2", Subtitle: "Sub hello 2", Content: "content2"},
+		}
+
+		client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://news-api:qwertyuiop@cluster0.gv8ol.mongodb.net/test?retryWrites=true&w=majority"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		err = client.Connect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer client.Disconnect(ctx)
+
+		quickstartDb := client.Database("news")
+		articlesCollection := quickstartDb.Collection("articles")
+
+		articleResult, err := articlesCollection.InsertMany(ctx, []interface{}{
+			bson.D{
+				{Key: "id", Value: "1"},
+				{Key: "title", Value: "hello"},
+				{Key: "subtitle", Value: "hello sub"},
+				{Key: "content", Value: "content"},
+			},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Inseted %v documents into article collection!\n", len(articleResult.InsertedIDs))
+		//err = client.Ping(ctx, readpref.Primary())
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+
+		handleRequests()
+	}
+*/
--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the news api")
-	//fmt.Println("endpoint hit: homePage")
+	fmt.Println("endpoint hit: homePage")
 }
 
 func handleRequests() {
@@ -36,22 +36,7 @@ type Article struct {
 	//CreationTime
 }
 
-/*
-type Article struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title    string             `json: "title,omitempty" bson:"title,omitempty"`
-	Subtitle string             `json: "subtitle,omitempty" bson:"subtitle,omitempty"`
-	Content  string             `json: "content,omitempty" bson:"content,omitempty"`
-}
-*/
-
 var Articles []Article
-
-func returnArticle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("endpoint hit: returnArticle")
-
-	json.NewEncoder(w).Encode(Articles)
-}
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
@@ -136,43 +121,5 @@ func CreateArticle(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	Articles = []Article{
-		Article{Id: "1", Title: "Hello", Subtitle: "Sub hello", Content: "content1"},
-		Article{Id: "2", Title: "Hello 2", Subtitle: "Sub hello 2", Content: "content2"},
-	}
-
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://news-api:qwertyuiop@cluster0.gv8ol.mongodb.net/test?retryWrites=true&w=majority"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer client.Disconnect(ctx)
-
-	quickstartDb := client.Database("news")
-	articlesCollection := quickstartDb.Collection("articles")
-
-	articleResult, err := articlesCollection.InsertMany(ctx, []interface{}{
-		bson.D{
-			{Key: "id", Value: "1"},
-			{Key: "title", Value: "hello"},
-			{Key: "subtitle", Value: "hello sub"},
-			{Key: "content", Value: "content"},
-		},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Inseted %v documents into article collection!\n", len(articleResult.InsertedIDs))
-	//err = client.Ping(ctx, readpref.Primary())
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
 	handleRequests()
 }
